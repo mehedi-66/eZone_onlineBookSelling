18a885e97bd763c994a7502744f2d10f19e39f9b
Mehedi Hasan <mehedisn3@gmail.com>
2023-01-16 19:32:44 +0600

    Home Page just finish

---
+++ eZone.Models/ShopingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eZone.Models
+{
+    public class ShopingCart
+    {
+        public Product Product { get; set; }
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+    }
+}
--- eZone/Areas/Customer/Controllers/HomeController.cs
+++ eZone/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using eZone.Models;
+﻿using eZone.DataAccess.Repository.IRepository;
+using eZone.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,21 +9,29 @@ namespace eZone.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> product = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(product);
         }
 
-        public IActionResult Privacy()
+
+        public IActionResult Details(int id)
         {
-            return View();
-        }
+            ShopingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category")
+             }; 
+            return View(cartObj);
+        } 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
